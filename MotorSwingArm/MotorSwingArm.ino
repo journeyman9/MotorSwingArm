987d3d7fa6b8c9cb07a11597db83d839ebb23070
journeyman9 <t4ke4journey@gmail.com>
2017-12-27 19:18:07 -0800

    Renamed main file and eliminated other files not in use

--- MotorSwingArm/MotorSwingArm.ino
+++ MotorSwingArm/MotorSwingArm.ino
@@ -1,58 +1,323 @@
 /*
-int main() {
-    //Timer 1, Mode 14 with ICR1 - period OCR1A/B - duty
-  TCCR1A = (1 << WGM11);
-  TCCR1B = (1 << WGM12) | (1 << WGM13) | (1 << CS11);
-  //set non-inverting
-  TCCR1A |= (1 << COM1B1);
-  //set period
-  ICR1 = 39999;
-  //set output to PB2 or pin 10
-  DDRB = (1 << 2);
-  char input;
+ * Tutorial 1a: Motor Driver
+ * 
+ * Drives two motors forward.
+ *
+ * The circuit:
+ *  - 6 pins connected to the L298 Motor Driver
+ *
+ * created 27 Sep 2013
+ * by Blaise Jarrett
+ *
+ * This example code is in the public domain.
+ *
+ */
+
+// include our motor code
+#include "motor.h"
+#include "Pot.h"
+#include "Arduino.h"
+
+// The Right Motors Enable Pin
+// Labelled on the motor driver as ENA
+// Be carful of PWM Timers
+const int motorRENPin = 10;
+// The Right Motors IN1 Pin
+// Labelled on the motor driver as IN2
+const int motorRIN2Pin = 9;
+// The Right Motors IN2 Pin
+// Labelled on the motor driver as IN1
+const int motorRIN1Pin = 8;
+
+// The Left Motors Enable Pin
+// Labelled on the motor driver as ENB
+// Be carful of PWM Timers
+const int motorLENPin = 5;
+// The Left Motors IN1 Pin
+// Labelled on the motor driver as IN3
+const int motorLIN2Pin = 7;
+// The Left Motors IN2 Pin
+// Labelled on the motor driver as IN4
+const int motorLIN1Pin = 6;
+
+// Create two Motor objects (instances of our Motor class)
+// to drive each motor.
+Motor rightMotor(motorRIN1Pin, motorRIN2Pin, motorRENPin);
+Motor leftMotor(motorLIN1Pin, motorLIN2Pin, motorLENPin);
+
+Pot feedback;
+
+uint32_t posSet;
+uint32_t pos;
+int32_t u;
+int32_t error;
+
+uint32_t serialdata;
+char serialCMD;
+int inbyte;
+int32_t P;
+int32_t I;
+int32_t D;
+int32_t N;
+int32_t W;
+int32_t A;
+int switchnum;
+
+void Command();
+
+void setup()
+{
+  //Switch Input
+  DDRD |= 0x00;
+  PORTD |= 0b00000100; // Secondary function to set pull up resistor
   Serial.begin(9600);
-  Serial.flush();
-  Serial.println("Press g for go and s for stop");
-  Serial.println("Press g for go and s for stop");
-  Serial.println("Press g for go and s for stop");
+  A = 90; //Define Angle as from 0 deg to 180deg. Assume nominal 90deg for vertical stick.
+ }
+
+void loop()
+{
+  if (Serial.available())
+ {
+   Command();
+ } 
+
+  posSet = 4.74*A + 127;
+  pos = feedback.readFeedback();
   
-    while (1) {
-      if (Serial.available()){
-        input = Serial.read();
-        Serial.print("I received: ");
-        Serial.println(input);
-      }
-            
-        if (input == 'g')
+  /* A2D min - 44
+   *  A2D max - 1005
+   *  A2D middle - 544
+   *  A2D 0 - 127
+   *  A2D 180 - 980
+   */
+
+   // Software limit switches for position
+   if ((pos >= 767) || (pos <= 340))
+   {
+    u = 0;
+    P = I = D = N = W = 0;
+    Serial.println("Fault detected, Motor Stopped");
+    _delay_ms(500);
+   }
+  error = posSet - pos;
+  u = P*error/100;
+
+  //Serial.print("u0: ");
+  //Serial.println(u);
+ 
+  if (u > 255)
+  {
+    u = 255;
+  }
+    else if (u < -255)
+    {
+      u = -255; 
+    }
+  if (u >= 0)
+  {
+    rightMotor.forward(u);
+  }
+    else if (u < 0)
+    {
+      u = -1*u;
+      rightMotor.backward(u);
+    } 
+/*
+  // delay for loop
+  Serial.print("u: ");
+  Serial.println(u);
+  Serial.print("error: ");
+  Serial.println(error);
+  Serial.println("------");
+  _delay_ms(1000); 
+*/
+}
+
+void Command()
+{
+  void getCommand();
+  void getSerial();
+  serialCMD = 0;
+  switchnum = 0;
+  getCommand();
+
+  if (serialCMD == 'R' || serialCMD == 'r')
+  {
+    switchnum = 1;
+  }
+  else if (serialCMD == 'P' || serialCMD == 'p')
+  {
+    switchnum = 2;
+  }
+  else if (serialCMD == 'I' || serialCMD == 'i')
+  {
+    switchnum = 3;
+  }
+  else if (serialCMD == 'D' || serialCMD == 'd')
+  {
+    switchnum = 4;
+  }
+  else if (serialCMD == 'N' || serialCMD == 'n')
+  {
+    switchnum = 5;
+  }
+  else if (serialCMD == 'W' || serialCMD == 'w')
+  {
+    switchnum = 6;
+  }
+  else if (serialCMD == 'A' || serialCMD == 'a')
+  {
+    switchnum = 7;
+  }
+  else
+  {
+    switchnum = 8;
+  }
+  
+switch(switchnum)
+{
+  case 1:
+       {
+     //Reset PID parameters
+          if(serialdata != 'E')
           {
-            OCR1B = 1199;
-            _delay_ms(500);
-            OCR1B = 2999;
-            _delay_ms(4000); 
-            Serial.println("Instruction Received: go");
+            P = I = D = N = W = 0;
+            A = 90;
+          }
+
+         break;  
+        }
+   case 2:
+        {
+          //update Proportional variable
+    
+          getSerial();
+          if(serialdata != 'E')
+          {
+            P = serialdata;
           }
           
-          if (input == 's')
+          break; 
+        }
+   case 3:
+        {
+          //Update Integral variable
+          getSerial();
+           if(serialdata != 'E')
           {
-            OCR1B = 0;
-            _delay_ms(500);
-            Serial.println("Instruction Received: stop");
+            I = serialdata;
           }
-     /*
-      //set duty cycle
-      OCR1B = 1199; // move to 600 us or 90 degrees left
-      _delay_ms(1000);
-      OCR1B = 2099; // move to 1050 us or 45 degrees left
-      _delay_ms(1000);
-      OCR1B = 2999; // move to 1500 us perpendicular
-      _delay_ms(1000);
-      OCR1B = 3899; // move to 1950 us or 45 degrees right
-      _delay_ms(1000);
-      OCR1B = 4799; // move to 2400 us or 90 degrees right
-      _delay_ms(1000);
-      */
-/*   
-    }
-return 0;
+          break;
+         
+        }
+        
+   case 4:
+       {
+         //Update Derivative variable
+          getSerial();
+          if(serialdata != 'E')
+          {
+            D = serialdata;
+          }
+          break;
+          
+       }
+       
+   case 5:
+       {
+       //Update Derivative filter constant
+         getSerial();
+          if(serialdata != 'E')
+          {
+            N = serialdata;
+          }
+         break; 
+       }
+   case 6:
+       {
+         //Update anti-Windup limit constant
+         getSerial();
+          if(serialdata != 'E')
+          {
+            W = serialdata;
+          }
+         break; 
+        
+       }
+     case 7:
+       {
+       
+          //Set target angle
+         getSerial();       
+          if(serialdata != 'E')
+          {
+            if (serialdata >= 0 && serialdata <= 180)
+             {               
+               A = serialdata;
+             }
+             else 
+             {
+               Serial.print("OUT OF RANGE");
+             }
+             
+         break;
+          }
+       } 
+     case 8:
+          {
+            Serial.println("Command ERROR!");
+          break;
+          }
 }
-*/
+       Serial.print("P: ");
+       Serial.print(P); 
+       Serial.print(" I: ");
+       Serial.print(I);
+       Serial.print(" D: ");
+       Serial.print(D);
+       Serial.print(" N: ");
+       Serial.print(N);
+       Serial.print(" W: ");
+       Serial.print(W);
+       Serial.print(" A: ");
+       Serial.println(A);
+}
+
+long getCommand()
+{
+  serialCMD = 0;
+  while (inbyte != '/')
+  {
+     inbyte = Serial.read();
+     if (inbyte > 0 && inbyte != '/')
+    {
+     serialCMD = inbyte;  
+    }
+  }
+  inbyte = 0;
+  return serialCMD;
+}
+
+long getSerial()
+{
+  serialdata = 0;
+  inbyte = 0;
+  while (inbyte != '\r')
+  {
+    inbyte = Serial.read(); 
+    
+    if (inbyte >= 48 && inbyte <= 57 && inbyte != '\r')
+    {
+      serialdata = serialdata * 10 + inbyte - '0';
+    }
+    else if (inbyte > 0 && inbyte < 48 && inbyte != '\r' || inbyte > 57  )
+    {
+        Serial.print("Num Error: ");
+        Serial.println(inbyte);
+        serialdata = 'E';
+        break;
+    }
+  }
+
+  return serialdata;
+}
